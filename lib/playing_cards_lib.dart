c527ab0f2f38cf75cc28df8df6483d0aea12fddf
Pintu Meena <pintulalmee@gmail.com>
2025-07-02 10:45:24 +0530

    Minor update for publishing/formatting

--- lib/playing_cards_lib.dart
+++ lib/playing_cards_lib.dart
@@ -1,14 +1,10 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-///library;
+/// A library for representing and manipulating playing cards.
+library ;
 
 export 'src/model/card_value.dart';
 export 'src/model/suit.dart';
 export 'src/model/playing_card.dart';
 export 'src/model/deck.dart';
 export 'src/utils/internal_playing_card_extensions.dart';
-
-
 
 // TODO: Export any libraries intended for clients of this package.
